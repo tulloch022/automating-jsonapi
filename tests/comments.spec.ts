8679a3ff9d00ffc6a31cb38567004b6e5a0c2ba1
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2025-01-02 20:39:38 -0800

    added assertion messages to comments endpoint tests

--- tests/comments.spec.ts
+++ tests/comments.spec.ts
@@ -21,11 +21,11 @@ test.describe('/comments API Endpoint Test', () => {
         const response = await request.get('/comments');
     
         // Verify status code is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code to be 2-- for /comments').toBe(200);
     
         // Verify response is not empty
         const responseBody: Comment[] = await response.json();
-        expect(responseBody.length).toBeGreaterThan(0);
+        expect(responseBody.length, 'Expected response to not be empty').toBeGreaterThan(0);
     
         // Verify structure of each comment
         responseBody.forEach(validateCommentStucture)
@@ -35,15 +35,15 @@ test.describe('/comments API Endpoint Test', () => {
         const response = await request.get('/comments?postId=2')
     
          // Verify status code is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code to be 200 for /comments?postId=2').toBe(200);
         
         // Verify response is not empty
         const responseBody: Comment[] = await response.json();
-        expect(responseBody.length).toBeGreaterThan(0);
+        expect(responseBody.length, 'Expected response to not be empty').toBeGreaterThan(0);
     
         // Verify all comments are from postId=2
         responseBody.forEach(comment => {
-            expect(comment.postId).toBe(2);
+            expect(comment.postId, 'Expected postId to be 2 for all comments').toBe(2);
         })
     })
     test('should return 0 comments', async ({ request }) => {
