d92b4cc86c3514c6d8712443381f62d872dbc664
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2025-01-02 20:26:05 -0800

    added assertion messages to posts endpoint tests

--- tests/posts.spec.ts
+++ tests/posts.spec.ts
@@ -8,10 +8,10 @@ interface Post {
 }
 
 const validatePostStructure = (post: Post) => {
-    expect(post.userId).toBeDefined();
-    expect(post.id).toBeDefined();
-    expect(post.title).toBeDefined();
-    expect(post.body).toBeDefined();
+    expect(post.userId, 'Post is missing userId').toBeDefined();
+    expect(post.id ,'Post is missing id').toBeDefined();
+    expect(post.title, 'Post is missing title').toBeDefined();
+    expect(post.body, 'Post is missing body').toBeDefined();
 }
 
 test.describe('/posts API Endpoint Tests', () => {
@@ -19,11 +19,11 @@ test.describe('/posts API Endpoint Tests', () => {
         const response = await request.get('/posts');
     
         // Verify status code is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code 200 for /posts').toBe(200);
     
         // Verify response body is not empty
         const responseBody: Post[] = await response.json();
-        expect(responseBody.length).toBeGreaterThan(0);
+        expect(responseBody.length, 'Expected response to not be empty').toBeGreaterThan(0);
     
         responseBody.forEach(validatePostStructure)
     });
@@ -33,7 +33,7 @@ test.describe('/posts API Endpoint Tests', () => {
         const response = await request.get('/posts?userId=1');
     
         // Verify status code is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code 200 for /posts?userId=1').toBe(200);
     
         // Verify response body is not empty
         const responseBody: Post[] = await response.json();
@@ -42,7 +42,7 @@ test.describe('/posts API Endpoint Tests', () => {
         // Verify only posts from user 1 are returned
         
         responseBody.forEach((post) => {
-            expect(post.userId).toBe(1);
+            expect(post.userId, 'Expected user ID to be 1').toBe(1);
         })
     })
 
@@ -50,18 +50,18 @@ test.describe('/posts API Endpoint Tests', () => {
         const response = await request.get('/posts?_limit=5');
 
         // Verify status code is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code to 200 for /posts?_limit=5').toBe(200);
 
         // Verify exactly 5 posts are returned
         const responseBody: Post[] = await response.json();
-        expect(responseBody.length).toBe(5);
+        expect(responseBody.length, 'Expected exactly 5 posts to returned').toBe(5);
     })
 
     test('should return posts in ascending order', async ({ request }) => {
         const response = await request.get('/posts?_sort=title&_order=asc');
 
         // Verify status code is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code to be 200 for /posts?_sort=title&_order=asc').toBe(200);
 
         // Sort the titles ascendingly programatically
         const responseBody: Post[] = await response.json();
@@ -69,20 +69,30 @@ test.describe('/posts API Endpoint Tests', () => {
 
         const sortedTitles = [...actualTitles].sort()
         
-        expect(actualTitles).toEqual(sortedTitles)
+        expect(actualTitles, 'Expected posts to be sorted in ascending order').toEqual(sortedTitles)
     })
     test('should return all posts with no limit', async ({ request }) => {
         // Provide invalid query parameter
         const response = await request.get('/posts?_limits=5');
 
         // Verify response status is good
-        expect(response.status()).toBe(200);
+        expect(response.status(), 'Expected status code to be 200 for invalid query parameter').toBe(200);
 
         // Verify no limit was applied and all posts returned
         const responseBody: Post[] = await response.json();
-        expect(responseBody.length).toBe(100);
+        expect(responseBody.length, 'Expected all 100 posts to be returned for invalid query parameter').toBe(100);
 
         // Verify structure of response
         responseBody.forEach(validatePostStructure)
+    })
+    test('should return 0 results', async ({ request }) => {
+        const response = await request.get('/posts?userId=999');
+
+        // Verify status of response is good
+        expect(response.status(), 'Expected status code to be 200 for userId=999').toBe(200);
+        
+        // Verify response is empty
+        const responseBody = await response.json();
+        expect(responseBody.length, 'Expected response to be empty').toBe(0);
     })
 });
