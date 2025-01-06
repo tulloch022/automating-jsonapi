91a4e9296f32cfff3027177d4026faadbdeb68ae
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2025-01-05 21:29:06 -0800

    created post test for albums endpoint

--- tests/albums.spec.ts
+++ tests/albums.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 interface Album {
     userId: number;
-    id: number;
+    id?: number;
     title: string;
 }
 
@@ -37,5 +37,22 @@ test.describe('/albums API Endpoint Tests', () => {
     test('should return status code 404', async ({ request }) => {
         const response = await request.get('/albums/999');
         expect(response.status()).toBe(404)
+    })
+    test('should create a new ablum with the provided data', async ({ request }) => {
+        const newAlbum: Album = {
+            userId: 3,
+            title: 'album name',
+        }
+        const response = await request.post('/albums', {
+            data: newAlbum,
+        })
+
+        // Verify response status is good
+        expect(response.status()).toBe(201);
+
+        // Verify structure of response
+        const responseBody: Album = await response.json();
+        expect(responseBody.userId).toBe(newAlbum.userId);
+        expect(responseBody.title).toBe(newAlbum.title);
     })
 })
