4be6b3018fea45a6bc8e7b90e7cfd58d73e9faa5
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2025-01-06 10:52:29 -0800

    created put test for albums endpoint

--- tests/albums.spec.ts
+++ tests/albums.spec.ts
@@ -38,7 +38,7 @@ test.describe('/albums API Endpoint Tests', () => {
         const response = await request.get('/albums/999');
         expect(response.status()).toBe(404)
     })
-    test('should create a new ablum with the provided data', async ({ request }) => {
+    test('should create a new ablum with the provided values', async ({ request }) => {
         const newAlbum: Album = {
             userId: 3,
             title: 'album name',
@@ -54,5 +54,24 @@ test.describe('/albums API Endpoint Tests', () => {
         const responseBody: Album = await response.json();
         expect(responseBody.userId).toBe(newAlbum.userId);
         expect(responseBody.title).toBe(newAlbum.title);
+    })
+    test('should update existing album with provided values', async ({ request }) => {
+        const updatedAlbum =  {
+            id: 1,
+            userId: 3,
+            title: 'new album name'
+        }
+        const response = await request.put('/albums/1', {
+            data: updatedAlbum,
+        })
+
+        // Verify response status is good
+        expect(response.status()).toBe(200);
+
+        // Verify structure of response
+        const responseBody: Album = await response.json();
+        expect(responseBody.id).toBe(updatedAlbum.id);
+        expect(responseBody.userId).toBe(updatedAlbum.userId);
+        expect(responseBody.title).toBe(updatedAlbum.title);
     })
 })
